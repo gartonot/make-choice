30703dac2d249cef6bcdbbb46b7f65763884b8f0
Gatonot <gartonot62@gmail.com>
2023-05-22 02:39:28 +0300

    feat: added UI and html home page

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import bridge from '@vkontakte/vk-bridge'
+import '@/assets/scss/global.scss'
 
 bridge.send('VKWebAppInit')
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,56 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import AppCard from '@/components/ui/AppCard.vue'
+  import AppButton from '@/components/ui/AppButton.vue'
+  import { computed } from 'vue'
+
+  const buttonLabel = computed(() => 'Далее')
+
+  const slideCard = (): void => {
+    console.log('click next')
+  }
+</script>
 
 <template>
   <div class="home">
-    <h1>Home page</h1>
+    <div class="card-list">
+      <app-card class="card-item">
+        Бывает сложно сделать какой-то выбор и мучаешь себя, что покушать, что надеть, что
+        посмотреть, проблема решена, просто сделай выбор
+      </app-card>
+      <app-card class="card-item">
+        Выбери категорию и то, какой вопрос тебя заставляет долго думать, приложение само предложит
+        интересные варианты для тебя!
+      </app-card>
+    </div>
+
+    <app-button :label="buttonLabel" class="home__button" @click="slideCard()" />
   </div>
 </template>
+
+<style lang="scss" scoped>
+  @import '@/assets/scss/variables.scss';
+
+  .card-list {
+    margin-top: 52px;
+    position: relative;
+  }
+  .card-item {
+    &:last-child {
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+  }
+
+  .home {
+    &__button {
+      margin-top: 24px;
+      width: 100%;
+      font-size: 20px;
+      line-height: 24px;
+
+      background: $secondary-color;
+      box-shadow: 0 3px 0 $secondary-darken-color;
+    }
+  }
+</style>
